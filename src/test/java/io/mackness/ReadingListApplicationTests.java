b72462c9b64c253de9848dfcc5c5639f837182b1
robertmackness <robert.mackness.gbr@gmail.com>
2016-06-04 17:58:22 +0100

    Add custom error page.

--- src/test/java/io/mackness/ReadingListApplicationTests.java
+++ src/test/java/io/mackness/ReadingListApplicationTests.java
@@ -1,15 +1,29 @@
 package io.mackness;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ReadingListApplication.class)
 @WebAppConfiguration
 public class ReadingListApplicationTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	private MockMvc mockMvc;
+
+	@Before
+	public void setupMockMVC(){
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 
 	@Test
 	public void contextLoads() {
